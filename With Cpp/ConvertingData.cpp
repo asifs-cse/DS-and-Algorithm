f60b2a60bcd6b9b1fad79512733a4f3567cd6f63
asifs-cse <asifs.cse@gmail.com>
2021-07-02 20:41:30 +0600

    frst

---
+++ With Cpp/ConvertingData.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binaryToDecimal(int n){
+    int x=1;
+    int ans =0;
+
+    while (n>0)
+    {
+        int y =n%10;
+        ans +=x*y;
+        x *=2;
+        n /=10;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<binaryToDecimal(n)<<endl;
+}
---
+++ With Cpp/Pythagoren.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<math.h>
+
+using namespace std;
+
+bool check(int x, int y, int z){
+    int a = max(x, max(y,z));
+    int b,c;
+    if(a==x)b=y, c=z;
+    else if(a==y) b=x, c=z;
+    else b=x, c=y;
+
+    if (a*a == b*b + c*c) return true;
+    else return false;
+}
+
+int32_t main(){
+    int x,y,z;
+    cin>>x>>y>>z;
+
+    if(check(x,y,z)){
+        cout<<"Pythagoren Triplet";
+    }else{
+        cout<<"Not Pythagoren Triplet";
+    }
+}
